3a4509383a854b9edb6a5807e035aaf1d6fbaa90
dhani <rahmad@mamiteam.com>
2021-08-18 17:46:47 +0800

    routing on app

--- src/App.js
+++ src/App.js
@@ -1,14 +1,29 @@
-import User from './components/User/User';
-import './App.css';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
+import PhotoDetails from "./pages/PhotoDetails/PhotoDetails";
+import PhotoList from "./pages/PhotoList/PhotoList";
+import PostDetails from "./pages/PostDetails/PostDetails";
+import UserList from './pages/UserList/UserList';
+import UserProfile from "./pages/UserProfile/UserProfile";
 
-function App() {
+export default function App() {
+  const NoMatchRoute = () => <div>404 Page</div>;
   return (
-    <div className="App">
-      <header className="App-header">
-        <User />
-      </header>
-    </div>
+    <Router>
+      <div>
+        <Switch>
+          <Route path="/" exact component={UserList} />
+          <Route path="/user/:id" exact component={UserProfile} />
+          <Route path="/user/posts/:id/comments" exact component={PostDetails} />
+          <Route path="/user/albums/:id/photos" exact component={PhotoList} />
+          <Route path="/user/albums/:id/photos/:id" exact component={PhotoDetails} />
+          <Route component={NoMatchRoute} />
+        </Switch>
+      </div>
+    </Router>
   );
 }
-
-export default App;
