47dacd0dd60b187f010db8273ba4a2f127d1897b
dhani <rahmad@mamiteam.com>
2021-09-01 15:42:19 +0800

    fetch api using promise and restyle post card

--- src/pages/UserProfile/UserProfile.js
+++ src/pages/UserProfile/UserProfile.js
@@ -5,26 +5,36 @@ import { Link } from "react-router-dom";
 import UserAlbum from '../UserAlbum/UserAlbum';
 // import axios from 'axios';
 // import UserPost from '../UserPost/UserPost'
+import './UserProfile.css';
 
 const UserProfile = ({ match }) => {
   const [post, setPost] = useState([]);
   const [album, setAlbum] = useState([]);
 
   useEffect(() => {
-    fetch('https://jsonplaceholder.typicode.com/posts?userId=' + match.params.id)
-      .then(res => res.json())
-      .then(result => setPost(result));
-    fetch('https://jsonplaceholder.typicode.com/albums?userId=' + match.params.id)
-      .then(res => res.json())
-      .then(result => setAlbum(result));
+    // fetch('https://jsonplaceholder.typicode.com/posts?userId=' + match.params.id)
+    //   .then(res => res.json())
+    //   .then(result => setPost(result));
+    // fetch('https://jsonplaceholder.typicode.com/albums?userId=' + match.params.id)
+    //   .then(res => res.json())
+    //   .then(result => setAlbum(result));
+    Promise.all([
+      fetch('https://jsonplaceholder.typicode.com/posts?userId=' + match.params.id),
+      fetch('https://jsonplaceholder.typicode.com/albums?userId=' + match.params.id)
+    ])
+      .then(([res1, res2]) => Promise.all([res1.json(), res2.json()]))
+      .then(([data1, data2]) => {
+        setPost(data1);
+        setAlbum(data2);
+      })
   });
 
   return (
-    <div>
+    <div className="profile-wrapper">
       THIS IS POSTS USER {match.params.id}
       <div>
         {post.map(item => (
-          <div key={item.id} className="user-wrapper">
+          <div key={item.id} className="post-wrapper">
             <h4>{item.title}</h4>
             <div>{item.body}</div>
             {/* <Link to={`/userProfile/${item.id}`} className="link"> */}
@@ -34,7 +44,6 @@ const UserProfile = ({ match }) => {
           </div>
         ))}
       </div>
-      <hr />
       <div>
         {album.map(item => (
           <UserAlbum id={item.id} title={item.title} item={item} />
