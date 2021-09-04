e00c4a740ea0637ab6fa63f4a0bb0db2ae1ecf2a
dhani <dhaniverdant23@gmail.com>
2021-09-04 15:40:41 +0800

    implement react tabs on user post and album

--- src/pages/UserProfile/UserProfile.js
+++ src/pages/UserProfile/UserProfile.js
@@ -1,9 +1,11 @@
 import React, { useState, useEffect } from 'react';
 import { Link, useHistory } from "react-router-dom";
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
+import 'react-tabs/style/react-tabs.css';
 import UserAlbum from '../UserAlbum/UserAlbum';
 import './UserProfile.scss';
 
-const UserProfile = ({ match }) => {
+const UserProfile = ({ match, name }) => {
   const [post, setPost] = useState([]);
   const [album, setAlbum] = useState([]);
 
@@ -24,22 +26,33 @@ const UserProfile = ({ match }) => {
     <div className="profile-wrapper">
       <button onClick={() => history.goBack()}>Back to Home</button>
       THIS IS POSTS USER {match.params.id}
-      <div>
-        {post.map(item => (
-          <div key={item.id} className="post-wrapper">
-            <h4>{item.title}</h4>
-            <div>{item.body}</div>
-            <Link to={`/user/posts/${item.id}/comments`} className="link">
-              <button>View Post Details</button>
-            </Link>
+      <Tabs>
+        <TabList>
+          <Tab>POST</Tab>
+          <Tab>ALBUM</Tab>
+        </TabList>
+
+        <TabPanel>
+          <div>
+            {post.map(item => (
+              <div key={item.id} className="post-wrapper">
+                <h4>{item.title}</h4>
+                <div>{item.body}</div>
+                <Link to={`/user/posts/${item.id}/comments`} className="link">
+                  <button>View Post Details</button>
+                </Link>
+              </div>
+            ))}
           </div>
-        ))}
-      </div>
-      <div>
-        {album.map(item => (
-          <UserAlbum id={item.id} title={item.title} item={item} />
-        ))}
-      </div>
+        </TabPanel>
+        <TabPanel>
+          <div>
+            {album.map(item => (
+              <UserAlbum id={item.id} title={item.title} item={item} />
+            ))}
+          </div>
+        </TabPanel>
+      </Tabs>
     </div>
   );
 }
